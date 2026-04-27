74975492286e0a5385d96ae48bba9fb92d3e7cae
mdjoyanes-lgtm <mdjoyanes@gmail.com>
2026-04-27 15:53:23 +0000

    cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// componente principal
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [light, setLight] = useState("red");
+
+	return (
+		<div className="text-center mt-5">
+
+			<div
+				style={{
+					background: "black",
+					width: "120px",
+					padding: "20px",
+					margin: "auto",
+					borderRadius: "15px"
+				}}
+			>
+
+				<div
+					onClick={() => setLight("red")}
+					style={{
+						background: "red",
+						width: "70px",
+						height: "70px",
+						borderRadius: "50%",
+						margin: "15px auto",
+						boxShadow:
+							light === "red"
+								? "0px 0px 25px red"
+								: "none"
+					}}
+				></div>
+
+				<div
+					onClick={() => setLight("yellow")}
+					style={{
+						background: "yellow",
+						width: "70px",
+						height: "70px",
+						borderRadius: "50%",
+						margin: "15px auto",
+						boxShadow:
+							light === "yellow"
+								? "0px 0px 25px yellow"
+								: "none"
+					}}
+				></div>
+
+				<div
+					onClick={() => setLight("green")}
+					style={{
+						background: "green",
+						width: "70px",
+						height: "70px",
+						borderRadius: "50%",
+						margin: "15px auto",
+						boxShadow:
+							light === "green"
+								? "0px 0px 25px green"
+								: "none"
+					}}
+				></div>
+
+			</div>
+
 		</div>
 	);
 };
